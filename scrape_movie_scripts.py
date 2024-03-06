7c75aba1ac0650502cf156f2085b58638af8faef
mbederman <mbederman02@gmail.com>
2024-03-06 14:17:02 -0500

    fixed script issue

--- scrape_movie_scripts.py
+++ scrape_movie_scripts.py
@@ -11,6 +11,8 @@ from pathlib import Path
 IMSDB_BASE_URL = "https://imsdb.com/alphabetical"
 KAGGLE_DATASET_CREDITS_PATH = "tmdb_movie_metadata/tmdb_5000_credits.csv"
 KAGGLE_DATASET_MOVIES_PATH = "tmdb_movie_metadata/tmdb_5000_movies.csv"
+SCRIPT_DIR = "scripts"
+ERROR_FILE_PATH = "no_scripts.txt"
 
 def initialize_webdriver():
     options = Options()
@@ -32,7 +34,7 @@ def scrape_available_movies(driver: webdriver.Chrome):
     letters = string.ascii_uppercase + '0'
     all_titles = []
     title_urls = []
-    for letter in tqdm(letters):
+    for letter in tqdm(letters[:1]):
         alphabet_path = Path(IMSDB_BASE_URL) / Path(letter)
         driver.get(str(alphabet_path))
         titles = driver.find_elements(By.CSS_SELECTOR, "td p a")
@@ -53,18 +55,50 @@ def load_movies_from_dataset():
 
     return titles
 
-if __name__ == "__main__":
-    movie_titles = load_movies_from_dataset()
-    driver = initialize_webdriver()
-    available_movies_titles, script_links = scrape_available_movies(driver)
-
+def find_matches(movie_titles, available_movie_titles, script_links):
     matches = []
     for movie_title in tqdm(movie_titles):
         closest = process.extractOne(movie_title, available_movies_titles)
         if closest[1] >= 95:
             matches.append((movie_title, script_links[available_movies_titles.index(closest[0])]))
 
-    print(len(matches))
+    return matches
+
+def scrape_scripts(driver: webdriver.Chrome, matches):
+    script_dir = Path(SCRIPT_DIR)
+    if not os.path.exists(script_dir):
+        os.mkdir(script_dir)
+
+    error_file_path = Path(ERROR_FILE_PATH)
+    if os.path.isfile(error_file_path):
+        os.remove(error_file_path)
+
+    for movie_title, script_link in tqdm(matches):
+        try:
+            driver.get(script_link)
+            driver.find_elements(By.CSS_SELECTOR, 'td a')[-1].click()
+            script = driver.find_element(By.CLASS_NAME, "scrtext").text
+
+            if len(script) <= 57:
+                raise Exception
+            
+            script_path = script_dir / Path(movie_title + ".txt")
+            with open(script_path, "w") as file:
+                file.write(script)
+        except Exception as e:
+            print(e)
+            with open(error_file_path, "a") as file:
+                file.write(movie_title + ", " + script_link + "\n")
+        
+        
+
+if __name__ == "__main__":
+    movie_titles = load_movies_from_dataset()
+    driver = initialize_webdriver()
+    available_movies_titles, script_links = scrape_available_movies(driver)
+    matches = find_matches(movie_titles, available_movies_titles, script_links)
+    scrape_scripts(driver, matches)
+    
 
     # script_dir = Path("scripts")
     # if not os.path.exists(script_dir):
